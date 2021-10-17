06f048b8a8f1fda59251e371d61b88e8b9411782
Chetan Badgujar <84768957+Chetanbadgujar53@users.noreply.github.com>
2021-10-17 11:01:26 +0530

    Update CalculatorTest.java

--- String-Calculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ String-Calculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -34,13 +34,13 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testNewLine()             //This menthod which is use for the testNewLine() ,whene the user enter the \n or ("1\n2,3") then it will calculator.add(("1\n2,3"))
+    public void testNewLine()          //This menthod which is use for the testNewLine() ,whene the user enter the \n or ("1\n2,3") then it will calculator.add(("1\n2,3"))
     {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
     @Test
-    public void testNegativeNumver         //This method which is use for the whene user entering NegativeNumver  then we developed this logic and also handling risky code with the help of try catch {} block , we hand
+    public void testNegativeNumver     //This method which is use for the whene user entering NegativeNumver  then we developed this logic and also handling risky code with the help of try catch {} block , we hand
     {
     	try {
 			Calculator.add("-1,2");
@@ -58,7 +58,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testOverThousand()               //This method which is use for the test over thousand();
+    public void testOverThousand()        //This method which is use for the test over thousand();
     {
     	assertEquals(2, Calculator.add("1000,2"));
     }
